e52e55d29566e45f125278760f354fd93831ec03
Emir Acar <105319468+EmirAcar06@users.noreply.github.com>
2023-06-05 11:59:38 +0300

    real estate game

---
+++ HaritaOyunuDeneme/build/classes/main/Main.rs
@@ -0,0 +1,14 @@
+main.Yonetici
+main.baglanti
+main.Main$1
+main.Main
+main.Main$4
+main.Arsa$1
+main.Main$2
+main.Main$3
+main.Arsa
+main.Kullanici$2
+main.Kullanici$1
+main.Kullanici
+main.Kullanici$3
+main.YoneticiCalisanlari
